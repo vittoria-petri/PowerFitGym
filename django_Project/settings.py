f4a8bd1c67e20999b53a98326ada4360e1e9d9ad
Vittoria Petri <109807672+vittoria-petri@users.noreply.github.com>
2023-06-15 16:49:17 +0200

    Final3

--- django_Project/settings.py
+++ django_Project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-=a!n%4snx0+s$=-vqyyim3efd&@aj$#4pqg9056a%bu4(wiwxk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = "static/"  # new
+STATIC_FILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
